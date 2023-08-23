4d83a722a94178091f80e4fb1f9b63ce06b97f67
Anjali Arya <anjalisinha498@gmail.com>
2023-08-23 23:15:02 +0530

    login,signup and profile component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,2 +1,38 @@
-import { initScriptLoader } from "next/script"
+import React from "react"
 
+export const LoginPage = () => {
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    })
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <hr />
+        
+        <label htmlFor="email">email</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="email"
+            type="text"
+            value={user.email}
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+            />
+        <label htmlFor="password">password</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="password"
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+            />
+            <button
+            // onClick={onLogin}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
+            {/* <Link href="/signup">Visit Signup page</Link> */}
+        </div>
+        )
+
+}
